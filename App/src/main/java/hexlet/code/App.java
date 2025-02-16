0c5a203d82b43c9575cb059e68444157da27ab6f
Eeetti <152491860+Eeetti@users.noreply.github.com>
2025-02-16 16:44:01 +0300

    add final bild1.001

--- App/src/main/java/hexlet/code/App.java
+++ App/src/main/java/hexlet/code/App.java
@@ -1,4 +1,5 @@
 package hexlet.code;
+
 import hexlet.code.Games.Welcome;
 import hexlet.code.Games.Even;
 import hexlet.code.Games.Cals;
@@ -12,53 +13,46 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        boolean stop = true;
-        while (stop) {
-            System.out.println("""
-                    Please enter the game number and press Enter.
-                    1 - Greet
-                    2 - Even
-                    3 - Calc
-                    4 - GCD
-                    5 - Progression
-                    6 - Prime
-                    0 - Exit"""
-            );
-            System.out.print("Your choice: ");
-            Scanner scan = new Scanner(System.in);
-            int numberGame = scan.nextInt();
-            System.out.println();
-            switch (numberGame) {
-                case 1:
-                    Welcome.welcome();
-                    break;
-                case 2:
-                    Even.welcome();
-                    Even.even();
-                    break;
-                case 3:
-                    Cals.welcome();
-                    Cals.cals();
-                    break;
-                case 4:
-                    GCD.welcome();
-                    GCD.gcd();
-                    break;
-                case 5:
-                    Progression.welcome();
-                    Progression.progression();
-                    break;
-                case 6:
-                    Prime.welcome();
-                    Prime.primee();
-                    break;
-                case 0:
-                    stop = false;
-                    System.out.println("Goodbye!");
-                    break;
-                default:
-                    break;
-            }
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calc
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit"""
+        );
+        System.out.print("Your choice: ");
+        Scanner scan = new Scanner(System.in);
+        int numberGame = scan.nextInt();
+        System.out.println();
+        switch (numberGame) {
+            case 1:
+                Welcome.welcome();
+                break;
+            case 2:
+                Even.welcome();
+                Even.even();
+                break;
+            case 3:
+                Cals.welcome();
+                Cals.cals();
+                break;
+            case 4:
+                GCD.welcome();
+                GCD.gcd();
+                break;
+            case 5:
+                Progression.welcome();
+                Progression.progression();
+                break;
+            case 6:
+                Prime.welcome();
+                Prime.primee();
+                break;
+            default:
+                break;
         }
     }
 }
